68d23801392b4f51dd468eb25a089bfa263edb14
iamberg <46677534+iamberg@users.noreply.github.com>
2021-02-12 08:52:04 +0100

    Add python testresources as dependency

--- scripts/before_install_apt_dependencies.sh
+++ scripts/before_install_apt_dependencies.sh
@@ -5,7 +5,7 @@ sudo apt -y autoremove
 sudo apt update
 
 sudo apt install -y software-properties-common
-sudo apt install -y python3-setuptools python3-apt python3-pip
+sudo apt install -y python3-setuptools python3-apt python3-pip python3-testresources
 
 sudo apt install -y yamllint
 sudo apt install -y shellcheck
